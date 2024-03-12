7c4899234fc23b42f0f63a270bdec941793ab2ac
emre381 <cihatemre381@gmail.com>
2024-03-12 15:41:21 +0300

    Proje dosyası ekle.

---
+++ MuiSortingApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MuiSortingApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MuiSortingApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿namespace MuiSortingApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Sorting_Button_Clicked(object sender, EventArgs e)
+        {
+            float[] X = new float[5];
+
+            float.TryParse(X1.Text, out X[0]);
+            float.TryParse(X2.Text, out X[1]);
+            float.TryParse(X3.Text, out X[2]);
+            float.TryParse(X4.Text, out X[3]);
+            float.TryParse(X5.Text, out X[4]);
+            
+            Array.Sort(X);
+            X1.Text = X[0].ToString();
+            X2.Text = X[1].ToString();
+            X3.Text = X[2].ToString();
+            X4.Text = X[3].ToString();
+            X5.Text = X[4].ToString();
+
+        }
+
+        private void Sorting_Button2_Clicked(object sender, EventArgs e)
+        {
+            
+            float[] X = new float[5];
+
+            float.TryParse(X1.Text, out X[0]);
+            float.TryParse(X2.Text, out X[1]);
+            float.TryParse(X3.Text, out X[2]);
+            float.TryParse(X4.Text, out X[3]);
+            float.TryParse(X5.Text, out X[4]);
+
+            Array.Sort(X, (a, b) => b.CompareTo(a));
+            X1.Text = X[0].ToString();
+            X2.Text = X[1].ToString();
+            X3.Text = X[2].ToString();
+            X4.Text = X[3].ToString();
+            X5.Text = X[4].ToString();
+
+        }
+    }
+
+}
